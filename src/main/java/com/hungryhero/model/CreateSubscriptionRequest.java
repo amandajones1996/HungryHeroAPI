225a2ba74e333ce84c293a0995efa5ca4cea94a8
Amanda <jonesamanda996@gmail.com>
2023-08-03 20:31:43 -0400

    stripe API integration complete

--- src/main/java/com/hungryhero/model/CreateSubscriptionRequest.java
+++ src/main/java/com/hungryhero/model/CreateSubscriptionRequest.java
@@ -7,10 +7,11 @@ import java.util.List;
 public class CreateSubscriptionRequest {
     private String customerId;
     private  List<String> items;
-    private String cardNumber; // Add fields for capturing card details
-    private Integer expMonth;
-    private Integer expYear;
-    private String cvc;
+    private String defaultPaymentMethod;
+    // private String cardNumber; // Add fields for capturing card details
+    // private Integer expMonth;
+    // private Integer expYear;
+    // private String cvc;
 
 
     // public CreateSubscriptionRequest() {
@@ -45,38 +46,46 @@ public class CreateSubscriptionRequest {
         this.items = items;
     }
 
-    public String getCardNumber() {
-        return cardNumber;
+    // public String getCardNumber() {
+    //     return cardNumber;
+    // }
+
+    // public void setCardNumber(String cardNumber) {
+    //     this.cardNumber = cardNumber;
+    // }
+
+    // // Getter and setter methods for expMonth
+    // public Integer getExpMonth() {
+    //     return expMonth;
+    // }
+
+    // public void setExpMonth(Integer expMonth) {
+    //     this.expMonth = expMonth;
+    // }
+
+    // // Getter and setter methods for expYear
+    // public Integer getExpYear() {
+    //     return expYear;
+    // }
+
+    // public void setExpYear(Integer expYear) {
+    //     this.expYear = expYear;
+    // }
+
+    // // Getter and setter methods for cvc
+    // public String getCvc() {
+    //     return cvc;
+    // }
+
+    // public void setCvc(String cvc) {
+    //     this.cvc = cvc;
+    // }
+
+    public String getDefaultPaymentMethod() {
+        return defaultPaymentMethod;
     }
 
-    public void setCardNumber(String cardNumber) {
-        this.cardNumber = cardNumber;
-    }
-
-    // Getter and setter methods for expMonth
-    public Integer getExpMonth() {
-        return expMonth;
-    }
-
-    public void setExpMonth(Integer expMonth) {
-        this.expMonth = expMonth;
-    }
-
-    // Getter and setter methods for expYear
-    public Integer getExpYear() {
-        return expYear;
-    }
-
-    public void setExpYear(Integer expYear) {
-        this.expYear = expYear;
-    }
-
-    // Getter and setter methods for cvc
-    public String getCvc() {
-        return cvc;
-    }
-
-    public void setCvc(String cvc) {
-        this.cvc = cvc;
+    public void setDefaultPaymentMethod(String defaultPaymentMethod) {
+        this.defaultPaymentMethod = defaultPaymentMethod;
     }
 }
--- src/main/java/com/hungryhero/service/StripeService.java
+++ src/main/java/com/hungryhero/service/StripeService.java
@@ -10,10 +10,13 @@ import org.springframework.stereotype.Service;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Customer;
 import com.stripe.model.PaymentIntent;
-import com.stripe.model.PaymentMethod;
+// import com.stripe.model.PaymentMethod;
+import com.stripe.model.SetupIntent;
+// import com.stripe.model.PaymentMethod;
 // import com.stripe.model.Price;
 import com.stripe.model.Subscription;
-import com.stripe.param.CustomerCreateParams;
+// import com.stripe.net.RequestOptions;
+// import com.stripe.param.CustomerCreateParams;
 // import com.stripe.param.CustomerUpdateParams;
 import com.stripe.param.PaymentIntentCreateParams;
 // import com.stripe.param.PriceCreateParams;
@@ -23,6 +26,7 @@ import com.stripe.param.PaymentIntentCreateParams;
 // import com.stripe.param.PaymentMethodAttachParams;
 // import com.stripe.param.PaymentMethodCreateParams;
 // import com.stripe.param.SubscriptionCreateParams;
+import com.stripe.param.SetupIntentCreateParams;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -36,14 +40,75 @@ import java.util.Map;
 public class StripeService {
         
         public StripeService() {}
+        
+        // public String createPaymentMethod(String cardNumber, Integer expMonth, Integer expYear, String cvc) throws StripeException {
+        //         Map<String, Object> cardParams = new HashMap<>();
+        //         cardParams.put("number", cardNumber);
+        //         cardParams.put("exp_month", expMonth);
+        //         cardParams.put("exp_year", expYear);
+        //         cardParams.put("cvc", cvc);
 
-        public Customer createCustomer(String email, String name) throws StripeException {
-                CustomerCreateParams params = CustomerCreateParams.builder()
-                        .setEmail(email)
-                        .setName(name)
+        //         Map<String, Object> paymentMethodParams = new HashMap<>();
+        //         paymentMethodParams.put("type", "card");
+        //         paymentMethodParams.put("card", cardParams);
+
+        //         PaymentMethod paymentMethod = PaymentMethod.create(paymentMethodParams);
+
+        //         return paymentMethod.getId();
+        // }
+
+        // public void attachPaymentMethodToCustomer(String customerId, String paymentMethodId) throws StripeException {
+        //         Customer customer = Customer.retrieve(customerId);
+        
+        //         CustomerUpdateParams customerUpdateParams = CustomerUpdateParams.builder()
+        //                 .setInvoiceSettings(
+        //                         CustomerUpdateParams.InvoiceSettings.builder()
+        //                                 .setDefaultPaymentMethod(paymentMethodId)
+        //                                 .build()
+        //                 )
+        //                 .build();
+        
+        //         customer = customer.update(customerUpdateParams);
+        // }
+
+        
+        public Map<String, String> createSetupIntent(String customerId) throws StripeException {
+                // Specify the supported payment method types (in this case, only card)
+                List<Object> paymentMethodTypes = new ArrayList<>();
+                paymentMethodTypes.add("card");
+
+                // Create SetupIntent with the customer ID and payment method types
+                SetupIntentCreateParams params = SetupIntentCreateParams.builder()
+                        .setCustomer(customerId)
+                        .putExtraParam("payment_method_types", paymentMethodTypes)
                         .build();
 
-                return Customer.create(params);
+                SetupIntent setupIntent = SetupIntent.create(params);
+
+                // Get the PaymentMethod ID from the SetupIntent (It will be automatically attached to the Customer on successful setup)
+                String paymentMethodId = setupIntent.getPaymentMethod();
+
+                // Prepare the result map
+                Map<String, String> result = new HashMap<>();
+                result.put("setupIntentId", setupIntent.getId());
+                result.put("paymentMethodId", paymentMethodId);
+
+                // Return the SetupIntent ID and PaymentMethod ID
+                return result;
+        }
+                
+
+        public Map<String, String> createCustomer(String email, String name) throws StripeException {
+                Map<String, Object> customerParams = new HashMap<>();
+                customerParams.put("email", email);
+                customerParams.put("name", name);
+        
+                Customer customer = Customer.create(customerParams);
+                
+                Map<String, String> customerInfo = new HashMap<>();
+                customerInfo.put("customerId", customer.getId());
+                
+                return customerInfo;
         }
 
         public PaymentIntent createPaymentIntent(String customerId, long amount, String currency) throws StripeException {
@@ -56,49 +121,102 @@ public class StripeService {
 
                 return PaymentIntent.create(params);
         }
-        public Subscription createSubscription(String customerId, List<String> priceIds, String cardNumber, Integer expMonth, Integer expYear, String cvc) throws StripeException {
-                // Step 1: Create the Payment Method
-                Map<String, Object> cardParams = new HashMap<>();
-                cardParams.put("number", cardNumber);
-                cardParams.put("exp_month", expMonth);
-                cardParams.put("exp_year", expYear);
-                cardParams.put("cvc", cvc);
-                
-                Map<String, Object> paymentMethodParams = new HashMap<>();
-                paymentMethodParams.put("type", "card");
-                paymentMethodParams.put("card", cardParams);
-                
-                PaymentMethod paymentMethod = PaymentMethod.create(paymentMethodParams);
-                
-                // Step 2: Attach the Payment Method to the customer
-                Customer customer = Customer.retrieve(customerId);
-                Map<String, Object> customerParams = new HashMap<>();
-                Map<String, Object> invoiceSettings = new HashMap<>();
-                invoiceSettings.put("default_payment_method", paymentMethod.getId());
-                customerParams.put("invoice_settings", invoiceSettings);
-                
-                customer.update(customerParams);
-                
-                // Step 3: Create the subscription with the default payment method
+
+        // public Subscription createSubscription(String customerId, List<String> priceIds) throws StripeException {
+        //         List<Map<String, Object>> items = buildSubscriptionItems(priceIds);
+        
+        //         Map<String, Object> subscriptionParams = new HashMap<>();
+        //         subscriptionParams.put("customer", customerId);
+        //         subscriptionParams.put("items", items);
+        
+        //         return Subscription.create(subscriptionParams);
+        // }
+
+        // private List<Map<String, Object>> buildSubscriptionItems(List<String> priceIds) {
+        // List<Map<String, Object>> items = new ArrayList<>();
+        // for (String priceId : priceIds) {
+        //         Map<String, Object> item = new HashMap<>();
+        //         item.put("price", priceId);
+        //         items.add(item);
+        // }
+        // return items;
+        // }
+
+        public Subscription createSubscription(String customerId, List<String> priceIds, String defaultPaymentMethod) throws StripeException {
                 List<Map<String, Object>> items = buildSubscriptionItems(priceIds);
-                
+
                 Map<String, Object> subscriptionParams = new HashMap<>();
                 subscriptionParams.put("customer", customerId);
                 subscriptionParams.put("items", items);
-                
-                return Subscription.create(subscriptionParams);
+
+                if (defaultPaymentMethod != null && !defaultPaymentMethod.isEmpty()) {
+                subscriptionParams.put("default_payment_method", defaultPaymentMethod);
                 }
-                
-                private List<Map<String, Object>> buildSubscriptionItems(List<String> priceIds) {
+
+                return Subscription.create(subscriptionParams);
+        }
+
+        private List<Map<String, Object>> buildSubscriptionItems(List<String> priceIds) {
                 List<Map<String, Object>> items = new ArrayList<>();
                 for (String priceId : priceIds) {
-                        Map<String, Object> item = new HashMap<>();
-                        item.put("price", priceId);
-                        items.add(item);
+                Map<String, Object> item = new HashMap<>();
+                item.put("price", priceId);
+                items.add(item);
                 }
                 return items;
-                }            
+        }
+
+        
+        public void cancelSubscription(String subscriptionId) throws StripeException {
+                Subscription subscription = Subscription.retrieve(subscriptionId);
+                subscription.cancel();
+        }
+
 }
+        
+        // public Subscription createSubscription(String customerId, List<String> priceIds, String cardNumber, Integer expMonth, Integer expYear, String cvc) throws StripeException {
+        //         // Step 1: Create the Payment Method
+        //         Map<String, Object> cardParams = new HashMap<>();
+        //         cardParams.put("number", cardNumber);
+        //         cardParams.put("exp_month", expMonth);
+        //         cardParams.put("exp_year", expYear);
+        //         cardParams.put("cvc", cvc);
+                
+        //         Map<String, Object> paymentMethodParams = new HashMap<>();
+        //         paymentMethodParams.put("type", "card");
+        //         paymentMethodParams.put("card", cardParams);
+                
+        //         PaymentMethod paymentMethod = PaymentMethod.create(paymentMethodParams);
+                
+        //         // Step 2: Attach the Payment Method to the customer
+        //         Customer customer = Customer.retrieve(customerId);
+        //         Map<String, Object> customerParams = new HashMap<>();
+        //         Map<String, Object> invoiceSettings = new HashMap<>();
+        //         invoiceSettings.put("default_payment_method", paymentMethod.getId());
+        //         customerParams.put("invoice_settings", invoiceSettings);
+                
+        //         customer.update(customerParams);
+                
+        //         // Step 3: Create the subscription with the default payment method
+        //         List<Map<String, Object>> items = buildSubscriptionItems(priceIds);
+                
+        //         Map<String, Object> subscriptionParams = new HashMap<>();
+        //         subscriptionParams.put("customer", customerId);
+        //         subscriptionParams.put("items", items);
+                
+        //         return Subscription.create(subscriptionParams);
+        //         }
+                
+        //         private List<Map<String, Object>> buildSubscriptionItems(List<String> priceIds) {
+        //         List<Map<String, Object>> items = new ArrayList<>();
+        //         for (String priceId : priceIds) {
+        //                 Map<String, Object> item = new HashMap<>();
+        //                 item.put("price", priceId);
+        //                 items.add(item);
+        //         }
+        //         return items;
+        //         }            
+
         // public Subscription createSubscription(String customerId, List<String> priceIds, String cardNumber, Integer expMonth, Integer expYear, String cvc) throws StripeException {
         //         // Step 1: Create the Payment Method
         //         Map<String, Object> cardParams = new HashMap<>();
